180f3f2b91028edc012a76bdb979aa9f897eefd5
Gustavo Bueno <89856818+GustavooBueno@users.noreply.github.com>
2021-09-08 12:13:58 -0300

    Explicando o projeto

--- Webcam.py
+++ Webcam.py
@@ -1,3 +1,7 @@
+#Esse é meu primeiro projeto usando a biblioteca Opencv.
+#Nesse projeto através do python é possível abrir a webcam e ver em tempo real as imagens da webcam.
+#Para fechar o programa basta clicar no ESC e além do programa ser fechado ele tira uma foto da última imagem que aparece na Webcam e salva como PNG.
+
 import cv2
 
 webcam = cv2.VideoCapture(0)
